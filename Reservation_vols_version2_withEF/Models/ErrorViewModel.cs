a16a7f2404827d7816dba004b55b003d5e51044c
Jerome Fourdrain <jerome.fourdrain@hotmail.com>
2022-05-10 16:33:47 +0200

    Ajoutez des fichiers projet.

---
+++ Reservation_vols_version2_withEF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reservation_vols_version2_withEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Reservation_vols_version2_withEF/Models/VwTicket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reservation_vols_version2_withEF.Models
+{
+    public partial class VwTicket
+    {
+        public int? Clientid { get; set; }
+        public string? Clientfirstname { get; set; }
+        public string? Clientlastname { get; set; }
+        public string? Clientaddress { get; set; }
+        public DateTime? Clientbirthdate { get; set; }
+        public string? Clientphone { get; set; }
+        public int? Passengerid { get; set; }
+        public string? Passengerfirstname { get; set; }
+        public string? Passengerlastname { get; set; }
+        public string? Passengeraddress { get; set; }
+        public DateTime? Passengerbirthdate { get; set; }
+        public string? Passengerphone { get; set; }
+        public int? Flightid { get; set; }
+        public bool? Isopen { get; set; }
+        public DateTime? DateDeparture { get; set; }
+        public DateTime? DateArrival { get; set; }
+        public int? AirportDepartureId { get; set; }
+        public int? AirportArrivalId { get; set; }
+    }
+}
